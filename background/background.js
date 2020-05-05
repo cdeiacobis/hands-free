cec97e1738ef9eb9c7a2a6d1e27c1d888affc9f6
nicolo.carpignoli <nicolo.carpignoli@chialab.it>
2020-05-05 10:04:10 +0200

    trying stackoverflow suggestion

---
+++ background/background.js
@@ -0,0 +1,49 @@
+chrome.runtime.onMessage.addListener(
+    function(request, sender, sendResponse) {
+        if (request.youCanAsk) {
+            handleCamera();
+            // sendResponse({ farewell: "goodbye" });
+        }
+    });
+
+const handleCamera = () => {
+    const hint = {
+        audio: false,
+        video: {
+            facingMode: 'environment',
+        },
+    };
+
+    const video = document.querySelector('video');
+
+    navigator.getUserMedia(hint, async (stream) => {
+        // Load the MediaPipe facemesh model assets.
+        const model = await facemesh.load();
+
+        // Pass in a video stream to the model to obtain
+        // an array of detected faces from the MediaPipe graph.
+        video.srcObject = stream;
+
+        // const track = stream.getTracks()[0];
+        // const settings = track.getSettings();
+        // const imageCapture = new ImageCapture(track);
+        // const canvas = new OffscreenCanvas(settings.width, settings.height);
+
+        requestAnimationFrame(nextFrame.bind(null, model));
+    }, (args) => { console.error(args) });
+
+    const nextFrame = async (model) => {
+        // const imageBitmap = await imageCapture.grabFrame();
+        // const context = canvas.getContext('2d');
+        // context.drawImage(imageBitmap, 0, 0);
+        // const imageData = context.getImageData(0, 0, canvas.width, canvas.height);
+
+        // console.log('imageData', imageData)
+        const faces = await model.estimateFaces(video);
+
+        // Each face object contains a `scaledMesh` property,
+        // which is an array of 468 landmarks.
+        faces.forEach(face => console.log(face.scaledMesh));
+        requestAnimationFrame(nextFrame.bind(null, model));
+    };
+};
--- utils/tf.js
+++ utils/tf.js
@@ -11,37 +11,8 @@
         },
     };
 
-    const video = document.querySelector('video');
-
-    navigator.getUserMedia(hint, async (stream) => {
-        // Load the MediaPipe facemesh model assets.
-        const model = await facemesh.load();
-
-        // Pass in a video stream to the model to obtain
-        // an array of detected faces from the MediaPipe graph.
-        video.srcObject = stream;
-
-        // const track = stream.getTracks()[0];
-        // const settings = track.getSettings();
-        // const imageCapture = new ImageCapture(track);
-        // const canvas = new OffscreenCanvas(settings.width, settings.height);
-
-        requestAnimationFrame(nextFrame.bind(null, model));
-    }, (err) => { console.error(err) });
-
-    const nextFrame = async (model) => {
-        // const imageBitmap = await imageCapture.grabFrame();
-        // const context = canvas.getContext('2d');
-        // context.drawImage(imageBitmap, 0, 0);
-        // const imageData = context.getImageData(0, 0, canvas.width, canvas.height);
-
-        // console.log('imageData', imageData)
-        const faces = await model.estimateFaces(video);
-
-        // Each face object contains a `scaledMesh` property,
-        // which is an array of 468 landmarks.
-        faces.forEach(face => console.log(face.scaledMesh));
-        requestAnimationFrame(nextFrame.bind(null, model));
-    };
+    chrome.runtime.sendMessage({ youCanAsk: true }, (response) => {
+        console.log('risposta', response);
+    });
 
 })();
